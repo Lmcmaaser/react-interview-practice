cd4ea8e1b7d96aab490e8328a6a9357b6713f593
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-02-28 13:22:20 -0700

    debugged routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-// import React from 'react';
 import './App.css';
-import React, {Component} from 'react';
-import {Route, BrowserRouter, Link} from 'react-router-dom';
-import HelloWorld from './HelloWorld.js';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import React, {Component} from 'react';
+import {Route, Switch, Link} from 'react-router-dom';
+//import HelloWorld from './HelloWorld.js';
 import HelloUser from './HelloUser.js';
 import Counter from './Counter.js'
 
 class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
                 <div className='App'>
                     <nav>
                         <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
                             <li>
                                 <Link to="/helloworld">HelloWorld</Link>
                             </li>
@@ -27,14 +24,19 @@ class App extends React.Component {
                             </li>
                         </ul>
                     </nav>
-                    <Route path="/helloworld/:helloWorldId" component={HelloWorld} />
-                    <Route path='/hellouser/:helloUserId' component={HelloUser} />
-                    <Route path='/counter/:counterId' component={Counter} />
-                </div>
-            </BrowserRouter>
-               
+                    <Switch>
+                        <Route exact path='/'/>
+                        {/* <Route exact path="/helloworld/:helloWorldId" component={HelloWorld} /> */}
+                        <Route path='/hellouser' component={HelloUser} />
+                        <Route path='/counter' component={Counter} />
+                    </Switch>
+                </div>     
         )
     }
 }
 
 export default App;
+
+/* questions:
+- shows appropriate links but does not connect to page
+*/
